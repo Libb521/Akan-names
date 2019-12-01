6dd38de0201d9ea9777fd0ad0523ce34158f4d77
Libb521 <eotieno39@yahoo.com>
2019-12-01 16:37:16 +0300

    I added a formulae that will be used  to finding the day of the week

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Afua","Afua","Ama"];
 
 
 //Get Input
-function getinput(){
+var day =function getinput(){
   century = parseInt(document.getElementById("century").value)
   year = parseInt(document.getElementById("year").value)
   month = parseInt(document.getElementById("month").value)
@@ -22,5 +22,12 @@ function getinput(){
   }else if(dayOfMonth==""){
   alert("Input correct date")
 }
+}
+function calculate(){
+  day();//call the function (day)
+  dayOfWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+  return (Math.floor(dayOfWeek));
+  console.log(dayOfWeek);
+  //This is to calculate and find the day of the week. Output should range from 0-6
 }
 
