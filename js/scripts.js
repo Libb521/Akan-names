cb53a072a284e6d258252ba0327ee9660ba76302
Libb521 <eotieno39@yahoo.com>
2019-12-02 17:10:02 +0300

    started and finished on a new js code to to ensure that the application works

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,47 @@
-function empty(){
-  return document.forms['form'];
-}
+function akanFunction() {
+  var ay, bm, cd;
+  var male, female;
 
-var a,b,c;
-empty()['btn'].addEventListener('click',function(e){
-e.preventDefault();
-checkEmpty();
-});
+  ay = parseInt(document.getElementById('year').value);
+  bm = parseInt(document.getElementById('month').value);
+  cd = parseInt(document.getElementById('dayOfMonth').value);
 
-function checkEmpty(){
-  function check(a){
-    return empty()[a].value
+  male = document.getElementById('rad1').checked;
+  female = document.getElementById('rad2').checked;
+
+  var year, month, day;
+  year = parseInt(ay);
+  month = parseInt(bm);
+  day = parseInt(cd);
+
+
+  var century = parseInt((year - 1) / 100 + 1);
+  var formula = (((26 * (month + 1) / 10)) + ((5 * year / 4)) + ((century / 4) - 2 * century - 1) + day) % 7;
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Afua", "Afua", "Ama"];
+  var dayOfMonth = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var dayIndex = Math.floor(formula);
+
+  if (year > 0 && year < 2020) {
+    if (month > 0 && month < 13) {
+      if (day > 0 && day < 32) {
+        //MALE NAMES
+        if (male) {
+          document.getElementById("result").innerHTML = (' You were born on a: ' + dayOfMonth[dayIndex] + ' your Akan name is ' + maleNames[dayIndex]);
+        } else {
+          //FEMALE NAMES
+          document.getElementById("result").innerHTML = (' You were born on a: ' + dayOfMonth[dayIndex] + ' your Akan name is ' + femaleNames[dayIndex]);
+        }
+
+      } else {
+        alert("day");
+      }
+
+    } else {
+      alert("month");
+    }
+
+  } else {
+    alert("Year");
   }
-
-  var year, month, dayOfMonth,gender;
-  yr =check('year');
-  mth=check('month');
-  dayOfMonth=check('dayOfMonth');
-  gender= check('gender')
-  if (year=='' && month=='' && dayOfMonth=='' && gender==''){
-  alert('Input right number in the fields provided to calculate your Akan name');
-  return 0;
-  }else if(year==''){
-    alert('input the right year 2000');
-    return 0;
-  }else if(month==''){
-    alert('input the right month e.g 12');
-    return 0;
-  }else if(dayOfMonth==''){
-    alert('input the right date e.g 30');
-    return 0;
-  }else{
-    calculate(year, month, dayOfMonth, gender);
-  }
-}
-
-
-function calculate(year, month, dayOfMonth,gender){
-  year = perseInt(year);
-  month = perseInt(month);
-  dayOfMonth = perseInt(dayOfMonth);
-  var century=(y-1)/100+1;
-  var formula=parseInt(((26*(month+1)/10)) +((5*year/4)) +((century/4) -2* century - 1) +dayOfMonth)%7;
-  var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-  var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Afua","Afua","Ama"];
-  var dayOfMonth = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-if (month<1 || month>12){
-  alert('invalid month');
-  return 0;
-}else if(dayOfMonth<1 || dayOfMonth>31){
-  alert('invalid date')
-  return 0;
-}else if(year<1 || year>2020){
-  alert('invalid year')
-}
-
-if(gender=='male'){
-  gender = male;
-}else if(gender=='female'){
-  gender=female;
-}else{
-  alert('invalid gender');
-  return 0;
-}
-
-for(var a=0; a<7;a++){
-  if(Math.ceil(formula)==(a)){
-    document.getElementById("result").innerHTML=('You were born on a:' +day[a]+ 'your Akan name is '+ g[a]);
-    return 0;
-  }
-}
 }
